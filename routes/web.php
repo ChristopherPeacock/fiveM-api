bcdd0586b23973977394df4ad0a01ce5a7fba776
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2025-02-25 20:49:10 +0000

    connected QBCore database successfully

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    try {
+        $allFiveMPlayers = DB::table('players')->get();
+        return response()->json($allFiveMPlayers);
+    } catch (\Exception $e) {
+        return response()->json(['error' => 'Could not retrieve players.'], 500);
+    }
 });
+
 
 require __DIR__.'/auth.php';
